0ad0c6451b9ad0aac4a10accf491b6b394ddec76
devincaughey <devin.caughey@gmail.com>
2023-03-22 22:45:47 -0400

    Update dynirt.R

--- R/dynirt.R
+++ R/dynirt.R
@@ -2,30 +2,83 @@
 #'
 #' Fit a dynIRT model using stan
 #'
-#' @param data (list) Data formatted for Stan (typically created by `shape()`).
+#' @param data (list) Data formatted for Stan, typically created by `shape()`.
 #' @param chains (positive integer) The number of Markov chains to run. The
 #'     default is 4.
-#' @param parallelize_within_chains
-#' @param threads_per_chain
-#' @param force_recompile
-#' @param init_kappa
-#' @param init
-#' @param return_data
-#' @param n_dim
-#' @param constant_alpha
-#' @param separate_eta
-#' @param lambda_zeros
-#' @param df_sigma_metric
-#' @param df_sigma_alpha_evol
-#' @param df_sigma_eta_evol
-#' @param mu_sigma_metric
-#' @param mu_sigma_alpha_evol
-#' @param mu_sigma_eta_evol
-#' @param sd_sigma_metric
-#' @param sd_sigma_alpha_evol
-#' @param sd_sigma_eta_evol
-#' @param seed An integer vector of length one indicating the state of Stan’s pseudo-random number generator
-#' @param ...
+#' @param parallelize_within_chains (logical) Should computations in a given
+#'     Markov chain be parallelized using Stan's `reduce_sum()` function?
+#'     Defaults to `FALSE`. If `TRUE`, then `threads_per_chain` should be set as
+#'     well. Since speed gains/losses from parallelization are highly
+#'     problem-specific, it is advisable to first experiment with test runs of a
+#'     few iterations.
+#' @param threads_per_chain (positive integer) Number of parallel processes to
+#'     use for within-chain parallelization. Ignored if
+#'     `parallelize_within_chains = FALSE`.
+#' @param force_recompile (logical) Should `cmdstanr::compile()` be required to
+#'     recompile the Stan model? Defaults to `FALSE`.
+#' @param init_kappa (logical) Should initial values for the item thresholds
+#'     `kappa` be pre-generated? This may reduce warnings of starting values
+#'     being rejected due to incompatibility with the order constraints on
+#'     `kappa`. Ignored unless `init = NULL`.
+#' @param init (multiple options) The initialization method to use for the
+#'     variables declared in the parameters block of the Stan program. For for
+#'     details, see the documentation for `cmdstanr::sample()`.
+#' @param return_data (logical) Should the data list fed to
+#'     `cmdstanr::sample()`, which includes additional elements not included in
+#'     the output of `shape()`, be returned with the fitted model? Defaults to
+#'     `TRUE`.
+#' @param n_dim (positive integer) Number of latent factors (i.e., dimensions)
+#'     in the factor model. Defaults to `1`.
+#' @param constant_alpha (logical) Should the item thresholds (`kappa`) be held
+#'     constant across time periods? If `FALSE` (the default), the thresholds
+#'     for a given item will be allowed to shift by a constant amount (governed
+#'     by `alpha`) in each time period.
+#' @param separate_eta (logical) Should units' factor scores (`eta`) be assigned
+#'     the same priors in every time period? Defaults to `TRUE`. If `FALSE`, the
+#'     scores in each period will be given priors centered on their value in the
+#'     previous period, thus smoothing the estimates across periods.
+#' @param lambda_zeros (multiple options) Should some item loadings (`lambda`)
+#'     be fixed at 0, and if so which ones? Rotational invariance (label
+#'     switching) across latent factors can be avoided by setting, for each
+#'     factor $d$, $d-1$ loadings to 0. If `lambda_zeros = NULL` (the default),
+#'     factor-specific parameters (e.g., `eta`) will not be identified, and the
+#'     draws will have to be rotated after sampling using `identify_draws(.,
+#'     rotate = TRUE)`. If `lambda_zeros = TRUE`, rotational identification will
+#'     be imposed by automatically choosing $d-1$ loadings to set to 0. Users
+#'     can also choose which loadings to restrict by inputting a two-column
+#'     character matrix, each row of which corresponds to a restriction. The
+#'     first column should be the name of an item, and the second column should
+#'     be a dimension number (as a character, e.g., `"2"`).
+#' @param df_sigma_metric (positive real) Degrees of freedom of the Student's t
+#'     prior for `sigma_metric`, the residual standard deviations of the metric
+#'     items. Defaults to 4.
+#' @param df_sigma_alpha_evol (positive real) Degrees of freedom of the
+#'     Student's t prior for `sigma_alpha_evol`, the standard deviation of the
+#'     dynamic prior for `alpha`. Defaults to 4.
+#' @param df_sigma_eta_evol (positive real) Degrees of freedom of the (half)
+#'     Student's t prior for `sigma_eta_evol`, the standard deviation of the
+#'     dynamic prior for `eta`. Defaults to `4`.
+#' @param mu_sigma_metric (real) Mean of the (half) Student's t prior for
+#'     `sigma_alpha_evol`, the residual standard deviations of the metric
+#'     items. Defaults to `0.5`.
+#' @param mu_sigma_alpha_evol (real) Mean of the (half) Student's t prior for
+#'     `sigma_alpha_evol`, the standard deviation of the dynamic prior for
+#'     `alpha`. Defaults to `0.1`.
+#' @param mu_sigma_eta_evol (real) Mean of the (half) Student's t prior for
+#'     `sigma_eta_evol`, the standard deviation of the dynamic prior for
+#'     `eta`. Defaults to `0.1`.
+#' @param sd_sigma_metric (positive real) Standard deviation of the (half)
+#'     Student's t prior for `sigma_alpha_evol`, the residual standard
+#'     deviations of the metric items. Defaults to `0.5`.
+#' @param sd_sigma_alpha_evol (positive real) Standard deviation of the
+#'     Student's t prior for `sigma_alpha_evol`, the standard deviation of the
+#'     dynamic prior for `alpha`. Defaults to `0.1`.
+#' @param sd_sigma_eta_evol (positive real) Standard deviation of the (half)
+#'     Student's t prior for `sigma_alpha_evol`, the residual standard
+#'     deviations of the metric items. Defaults to `0.1`.
+#' @param seed (positive integer) An integer vector of length one indicating the
+#'     state of Stan’s pseudo-random number generator. Defaults to `123`.
+#' @param ... Additional arguments to `cmdstanr::sample()`.
 #'
 #' @return A dynIRT object containing
 #'  \describe{
@@ -86,21 +139,21 @@ fit <- function (data,
     data = 1,
     nrow = data$I_binary,
     ncol = data$D,
-    dimnames = list(attr(data, "binary_item_labels"), NULL)
+    dimnames = list(attr(data, "binary_item_labels"), as.character(1:data$D))
   )
 
   nonzero_ordinal <- matrix(
     data = 1,
     nrow = data$I_ordinal,
     ncol = data$D,
-    dimnames = list(attr(data, "ordinal_item_labels"), NULL)
+    dimnames = list(attr(data, "ordinal_item_labels"), as.character(1:data$D))
   )
 
   nonzero_metric <- matrix(
     data = 1,
     nrow = data$I_metric,
     ncol = data$D,
-    dimnames = list(attr(data, "metric_item_labels"), NULL)
+    dimnames = list(attr(data, "metric_item_labels"), as.character(1:data$D))
   )
 
   if (data$D > 1 && isTRUE(lambda_zeros)) {
